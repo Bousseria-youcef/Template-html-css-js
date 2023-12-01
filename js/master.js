650d27a63fa44bb0009a964ee5e0aed382a47d38
Bousseria-youcef <youcefbousseria@outlook.fr>
2023-12-01 10:46:08 +0100

    create the Settings Box

--- js/master.js
+++ js/master.js
@@ -1,28 +1,32 @@
 // toggle sping class on icon
 
 document.querySelector(".toggle-settings .fa-gear").onclick = function () {
-    this.classList.toggle("fa-spin");
     
+this.classList.toggle("fa-spin");  
 //toggle class open on main setting box
-
 document.querySelector(".settings-box").classList.toggle("open");
 };
 
-
-
-
-
 let landingPage = document.querySelector(".landing-page");
-
-
 let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+//Switch colors
+const colorsli = document.querySelectorAll(".colors-list li");
+
+colorsli.forEach( li => {
+    li.addEventListener("click", (e) => {
+
+        
+        //set color on root
+        document.documentElement.style.setProperty('--main--color', e.target.dataset.color);
+    });
+
+} );
 
 
 
 
 //Get Random Number
-  
-
 
 setInterval(() => {
 
